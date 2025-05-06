ae8b18367476208a26cd1a8c79dbffc0c968c36a
wartman <faxmaster@gmail.com>
2025-05-06 13:16:02 -0500

    Remove useless code

--- src/capsule/When.macro.hx
+++ src/capsule/When.macro.hx
@@ -1,18 +1,13 @@
 package capsule;
 
-import haxe.macro.Context;
-import haxe.macro.Expr;
 import capsule.internal.Builder;
+import haxe.macro.Expr;
 
 using haxe.macro.TypeTools;
 
 class When {
 	public static function resolved(self:Expr, transform:Expr):Expr {
 		var factory = createFactory(transform, transform.pos);
-		var t = switch Context.typeof(self) {
-			case TInst(_, [t]): t.toComplexType();
-			default: macro :Dynamic;
-		}
 		return macro @:privateAccess @:pos(self.pos) $self.applyTransform(@:pos(transform.pos) function(value, container) {
 			return ${factory}(container);
 		});
